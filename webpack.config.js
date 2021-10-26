563b97531ead34d1a0a92f9ac2e239c1d15a9fe6
deepakbhattmits <deepakbhattmits@gmail.com>
2021-10-26 08:13:54 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from 'path'
+
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'src'),
+    },
+    compress: true,
+    hotOnly: true,
+    port: 3000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+}
